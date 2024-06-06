60d683dbf8f1722cdc4caad3487c03b341966a44
RaphaelRodrigues <114110428+RaphaelRSantos02@users.noreply.github.com>
2024-06-06 11:22:00 -0300

    06-06-24v1

---
+++ App.js
@@ -0,0 +1,9 @@
+import Geo from "./pages/geo";
+import Header from "./pages/geo/header";
+
+export default function App() {
+  return (
+    <Geo/>
+  );
+}
+
---
+++ pages/geo/header.jsx
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import { View, TouchableOpacity, Image, Text, StyleSheet } from 'react-native';
+
+// const Header = ({ onPressMenu }) => {
+//   return (
+//     <View style={styles.container}>
+//       <TouchableOpacity onPress={onPressMenu} style={styles.menuButton}>
+//         <Image source={require('../../assets/menu_icon.svg')} style={styles.icon} />
+//       </TouchableOpacity>
+//       <Text style={styles.logo}>Mapa Senai</Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//     paddingHorizontal: 20,
+//     paddingTop: 40,
+//     paddingBottom: 10,
+//     backgroundColor: '#f4511e', // Cor de fundo do header
+//   },
+//   menuButton: {
+//     padding: 10,
+//   },
+//   icon: {
+//     width: 30,
+//     height: 30,
+//     tintColor: '#fff', // Cor do ícone do menu
+//   },
+//   logo: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//     color: '#fff',
+//   },
+// });
+
+// export default Header;
